bb636ce65c1b76d69560db7aca40ac9184072072
Piotr Smoła <128245165+PiotrSmola@users.noreply.github.com>
2023-04-18 15:38:33 +0200

    Add files via upload

---
+++ Lab04/zad3.java
@@ -0,0 +1,8 @@
+public class zad3 {
+    public static void main(String[] args) {
+        String[] tablica = {"Ala", "ma", "kota", "i", "złotą", "rybkę"};
+        for (String slowo : tablica) {
+            System.out.print(slowo.toUpperCase()+"  ");
+        }
+    }
+}
