7bde3a811a568ace4e1853264fd9233095382e39
Piotr Smoła <128245165+PiotrSmola@users.noreply.github.com>
2023-05-11 20:37:56 +0200

    Update Main.java

--- Lab01/Main.java
+++ Lab01/Main.java
@@ -6,40 +6,40 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        System.out.println("zadanie 1");
+        System.out.println("excercise 1");
         System.out.println(zad1());
 
-        System.out.println("zadanie 2");
-        System.out.println("wprowadź pierwszą liczbę");
+        System.out.println("excercise 2");
+        System.out.println("enter the first number");
         float a1 = in.nextFloat();
-        System.out.println("wprowadź drugą liczbę");
+        System.out.println("enter the second number");
         float b1 = in.nextFloat();
         zad2(a1,b1);
 
-        System.out.println("zadanie 3");
-        System.out.println("wprowadź liczbę");
+        System.out.println("excercise 3");
+        System.out.println("enter a number");
         int c = in.nextInt();
         System.out.println(zad3(c));
 
-        System.out.println("zadanie 4");
-        System.out.println("wprowadź liczbę");
+        System.out.println("excercise 4");
+        System.out.println("enter a number");
         int d = in.nextInt();
         System.out.println(zad4(d));
 
-        System.out.println("zadanie 5");
-        System.out.println("wprowadź liczbę");
+        System.out.println("excercise 5");
+        System.out.println("enter a number");
         double e = in.nextFloat();
         System.out.println(zad5(e));
 
-        System.out.println("zadanie 6");
-        System.out.println("wprowadź liczbę");
+        System.out.println("excercise 6");
+        System.out.println("enter a number");
         double f = in.nextDouble();
         System.out.println(zad6(f));
 
-        System.out.println("zadanie 7");
-        System.out.println("wprowadź dolny kres przedziału liczb do losowania");
+        System.out.println("excercise 7");
+        System.out.println("enter the lower limit of the range of numbers to be drawn");
         int a = in.nextInt();
-        System.out.println("wprowadź górny kres przedziału liczb do losowania");
+        System.out.println("enter the upper limit of the range of numbers to be drawn");
         int b = in.nextInt();
         if (a<b) {
             Random test = new Random();
@@ -50,7 +50,7 @@ public class Main {
         }
         else
         {
-            System.out.println("Podano zły zakres. Koniec programu");
+            System.out.println("Wrong range specified. Program end");
             System.exit(0);
         }
     }
@@ -63,9 +63,9 @@ public class Main {
 
     public static float zad2(float a, float b)
     {
-        System.out.format("Suma liczb "+a+" i "+b+" wynosi: %.2f\n",(a+b));
-        System.out.format("Różnica liczb "+a+" i "+b+" wynosi: %.2f\n",(a-b));
-        System.out.format("Iloczyn liczb "+a+" i "+b+" wynosi: %.2f\n",(a*b));
+        System.out.format("Sum of numbers "+a+" i "+b+" is equal: %.2f\n",(a+b));
+        System.out.format("Number difference "+a+" i "+b+" is equal: %.2f\n",(a-b));
+        System.out.format("product of numbers "+a+" i "+b+" is equal: %.2f\n",(a*b));
         return 0;
     }
     public static boolean zad3(int c)
