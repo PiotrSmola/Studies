f93748dc0cd6579f3468f6dc01442f2c9107f2ea
Piotr Smoła <128245165+PiotrSmola@users.noreply.github.com>
2023-05-11 20:47:09 +0200

    Update and rename zad6.java to ex6.java

--- Lab02/ex6.java
+++ Lab02/ex6.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
-public class zad6 {
+public class ex6 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         while (true) {
-            System.out.print("Podaj liczbę całkowitą: ");
+            System.out.print("Enter an integer: ");
             int liczba = input.nextInt();
             if (liczba < 0) {
                 break;
             }
         }
-        System.out.println("Koniec programu");
+        System.out.println("Program end");
     }
 }
