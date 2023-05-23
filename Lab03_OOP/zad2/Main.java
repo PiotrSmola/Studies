012c75d617bb6e18db31f7e32d4ec13d6a1d05c1
Piotr Smoła <128245165+PiotrSmola@users.noreply.github.com>
2023-05-23 21:08:19 +0200

    Add files via upload

---
+++ Lab03_OOP/zad2/Main.java
@@ -0,0 +1,12 @@
+package zad2;
+
+public class Main {
+    public static void main(String[] args) {
+        wieloryb wieloryb = new wieloryb();
+        wieloryb.plyn();
+        wieloryb.wynurz();
+        wieloryb.zanurz();
+        wieloryb.jedz();
+        wieloryb.wydalaj();
+    }
+}
