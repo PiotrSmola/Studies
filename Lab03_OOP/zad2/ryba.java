2ef8ec18f417ac8c1e0d21b6c609b15cd49609f1
Piotr Smoła <128245165+PiotrSmola@users.noreply.github.com>
2023-05-23 21:07:17 +0200

    Add files via upload

---
+++ Lab03_OOP/zad2/ryba.java
@@ -0,0 +1,14 @@
+package zad2;
+
+public abstract class ryba extends zwierze {
+    @Override
+    public void jedz() {
+        System.out.println("Ryba je");
+    }
+
+    @Override
+    public void wydalaj() {
+        System.out.println("Ryba wydala");
+    }
+}
+
