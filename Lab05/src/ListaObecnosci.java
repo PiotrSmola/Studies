b8f5d99f331e6d042a450a348cc9aa032b4a9ce1
Piotr Smoła <128245165+PiotrSmola@users.noreply.github.com>
2023-06-10 19:08:48 +0200

    Update ListaObecnosci.java

--- Lab05/src/ListaObecnosci.java
+++ Lab05/src/ListaObecnosci.java
@@ -5,6 +5,24 @@ public class ListaObecnosci {
         String url = "jdbc:sqlite:lista_obecnosci.db";
         return DriverManager.getConnection(url);
     }
-
+    public static void stworzTabele() {
+        String sql = "CREATE TABLE IF NOT EXISTS obecnosc (" +
+                "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "prowadzacy TEXT," +
+                "nazwa_przedmiotu TEXT," +
+                "imie_i_nazwisko TEXT," +
+                "nr_albumu TEXT," +
+                "data DATE," +
+                "czas_zalogowania TIME," +
+                "czas_wylogowania TIME," +
+                "dlugosc_sesji INTEGER)";
+        try (Connection polacz = getConnection();
+             Statement stmt = polacz.createStatement()) {
+            stmt.execute(sql);
+            System.out.println("Tabela z listą obecności została utworzona");
+        } catch (SQLException e) {
+            System.out.println("Wystąpił błąd podczas tworzenia tabeli z listą obecności: " + e.getMessage());
+        }
+    }
 
 }
